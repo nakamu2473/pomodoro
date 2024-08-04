43b2da0a956a55d9974df601f00cb7d4a3562e94
nakamu <oyukita56@gmail.com>
2024-08-04 14:12:51 +0900

    タイマー回数を修正

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -4,14 +4,17 @@
       <p v-else>休憩!</p>
       <p>集中{{ this.focusTime }}分</p>
       <p>休憩{{ this.isBreak }}分</p>
+      <p>{{this.pomodoroCount}}回目</p>
 
      <h1>{{ formattedTime }}</h1>
       <div>
-        <input >
-        <input >
+        <input v-model="inputFocusTime" placeholder="集中する時間">
+        <input v-model="inputIsBreak" placeholder="休憩する時間">
+        <input v-model="inputPomodoroCount" placeholder="繰り返す回数">
+
       </div>
       <button @click="startTimer" :disabled="running">Start</button>
-      <button @click="stopTimer" :disabled="!running">Stop</button>
+      <button @click="stopBtnTimer" :disabled="!running">Stop</button>
       <button @click="resetTimer">Reset</button>
     </div>
   </template>
@@ -22,11 +25,15 @@
       return {
         focusTime: 10,
         isBreak: 5,
+        pomodoroCount: 1,
+        inputFocusTime: "",
+        inputIsBreak: "",
+        inputPomodoroCount: null,
         time: 0,
         interval: null,
         running: false,
         toFocus: true,
-        minToSec: 60 // デバッグ中は1にすると楽
+        minToSec: 1 // デバッグ中は1にすると楽
       };
     },
     created() {
@@ -41,49 +48,55 @@
     },
     methods: {
       startTimer() {
-        console.log("startTimer")
-        console.log(this.toFocus)
-        console.log(this.running)
         if (!this.running && this.time > 0) {
             this.running = true;
             this.interval = setInterval(() => {
-            if (this.time > 0) {
-                console.log("startTimer_--")
-                this.time--;
-            } else {
-                console.log("startTimer_stop")
-                //this.time = this.isBreak * 60;
-                if( this.toFocus ){
-                    this.time = this.isBreak;
+                if (this.time > 0) {
+                    this.time--;
                 } else {
-                    this.time = this.focusTime;
+                    if( this.toFocus ){
+                        this.time = this.isBreak;
+                    } else {
+                        this.time = this.focusTime;
+                    }
+                    this.toFocus = !this.toFocus;
+                    this.pomodoroCompleted();
                 }
-                this.toFocus = !this.toFocus;
-                this.stopTimer();
-            }
             }, 1000);
         }
       },
-      stopTimer() {
-        console.log("stopTimer")
-        console.log(this.toFocus)
-        if (this.toFocus) {
-            if (this.running) {
-                this.running = false;
-                clearInterval(this.interval);
-            }
+
+      stopBtnTimer() {
+        this.running = false;
+        clearInterval(this.interval);
+      },
+      pomodoroCompleted() {
+        if (this.toFocus && this.running) {
+            // ポモドーロ1回分終了
+            this.pomodoroCount++;
+        }
+        if (this.toFocus && this.running && this.inputPomodoroCount < this.pomodoroCount) {
+            // ポモドーロ停止
+            this.running = false;
+            this.resetTimer()
+        } else {
+            // 集中終了
+            this.startTimer();
         }
       },
       resetTimer() {
-        console.log("resetTimer")
-        console.log(this.toFocus)
-        this.stopTimer();
-        //this.time = this.focusTime * 60;
+        // リセット
         this.time = this.focusTime;
+        this.pomodoroCount = 1;
+        this.inputFocusTime = null;
+        this.inputIsBreak = null;
+        this.inputPomodoroCount = null;
+        this.running = false;
+        clearInterval(this.interval);
       },
     },
     beforeDestroy() {
-      this.stopTimer();
+      this.pomodoroCompleted();
     },
   };
   </script>
